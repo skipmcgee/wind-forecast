3972ca81409a675420c5b9e573b188437bf80fd9
Skip McGee <70141823+skipmcgee@users.noreply.github.com>
2024-04-06 17:08:39 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,4 @@
 pip3 install --user virtualenv
 python3 -m venv ./venv
 source venv/bin/activate
-pip3 install flask-mysqldb
-pip3 install openmeteo-requests
-pip3 install requests-cache retry-requests numpy pandas
-pip3 install python-dotenv
+pip3 install -r ../requirements.txt
