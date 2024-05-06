1f139af50f0867587ffbd6547b3b46e2784c7998
Skip McGee <davidfranklinmcgee@gmail.com>
2024-05-05 20:28:08 -0600

    updating pages and adding wireframe

--- app.py
+++ app.py
@@ -17,35 +17,34 @@ def root():
     models_query = "SELECT * FROM Models;"
     locations_query = "SELECT * FROM Locations;"
     sensors_query = "SELECT * FROM Sensors;"
-    forecasts_query = "SELECT * FROM Forecasts;"
-    readings_query = "SELECT * FROM Readings;"
 
     models_results = db.execute_query(db_connection=db_connection, query=models_query).fetchall()
     locations_results = db.execute_query(db_connection=db_connection, query=locations_query).fetchall()
     sensors_results = db.execute_query(db_connection=db_connection, query=sensors_query).fetchall()
+    
+    return render_template("main.j2", models=models_results, locations=locations_results, sensors=sensors_results,)
+
+@app.route('/results')
+def result_info(from_date, to_date, sensor):
+    forecasts_query = "SELECT * FROM Forecasts;"
+    readings_query = "SELECT * FROM Readings;"
     forecasts_results = db.execute_query(db_connection=db_connection, query=forecasts_query).fetchall()
     readings_results = db.execute_query(db_connection=db_connection, query=readings_query).fetchall()
 
-    return render_template("main.j2", models=models_results, locations=locations_results, sensors=sensors_results, forecasts=forecasts_results, readings=readings_results)
-'''
-@app.route('/edit')
-def edit(model_string):
-    edit_query = f"SELECT * FROM {model_string};"
-    edit_results = db.execute_query(db_connection=db_connection, query=edit_query).fetchall()
-    return render_template("edit.j2", model_string=edit_results)
+    return render_template("results.j2", forecasts=forecasts_results, readings=readings_results)
 
-@app.route('/update')
-def update():
-#    edit_query = f"SELECT * FROM {model_string};"
-#    edit_results = db.execute_query(db_connection=db_connection, query=edit_query).fetchall()
-    return render_template("update.j2")
+@app.route('/editsensors')
+def edit_sensors():
+    edit_sensor_query = f"SELECT * FROM Sensors;"
+    edit_sensor_results = db.execute_query(db_connection=db_connection, query=edit_sensor_query).fetchall()
+    return render_template("editsensors.j2", model_string=edit_sensor_results)
 
-@app.route('/add')
-def add():
-#    edit_query = f"SELECT * FROM {model_string};"
-#    edit_results = db.execute_query(db_connection=db_connection, query=edit_query).fetchall()
-    return render_template("add.j2")
-'''
+@app.route('/editlocations')
+def edit_locations():
+    edit_loc_query = f"SELECT * FROM Locations;"
+    edit_loc_results = db.execute_query(db_connection=db_connection, query=edit_loc_query).fetchall()
+    return render_template("editsensors.j2", model_string=edit_loc_results)
+
     
 
 if __name__ == "__main__":
