8c8ba1bbc8ef0553153b2f1b551f098b85a337f6
Skip McGee <davidfranklinmcgee@gmail.com>
2024-05-05 20:56:57 -0600

    initial rough out of pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from markupsafe import Markup
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect
 from flask_mysqldb import MySQL
 import os
 import database.db_connector as db
@@ -12,19 +12,26 @@ db_connection = db.connect_to_database()
 entities_list = ['models', 'locations', 'sensors', 'forecasts', 'readings', ] 
 
 # Routes 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def root():
-    models_query = "SELECT * FROM Models;"
-    locations_query = "SELECT * FROM Locations;"
-    sensors_query = "SELECT * FROM Sensors;"
+    if request.method == "GET":
+        models_query = "SELECT * FROM Models;"
+        locations_query = "SELECT * FROM Locations;"
+        sensors_query = "SELECT * FROM Sensors;"
 
-    models_results = db.execute_query(db_connection=db_connection, query=models_query).fetchall()
-    locations_results = db.execute_query(db_connection=db_connection, query=locations_query).fetchall()
-    sensors_results = db.execute_query(db_connection=db_connection, query=sensors_query).fetchall()
-    
-    return render_template("main.j2", models=models_results, locations=locations_results, sensors=sensors_results,)
+        models_results = db.execute_query(db_connection=db_connection, query=models_query).fetchall()
+        locations_results = db.execute_query(db_connection=db_connection, query=locations_query).fetchall()
+        sensors_results = db.execute_query(db_connection=db_connection, query=sensors_query).fetchall()
+        
+        return render_template("main.j2", models=models_results, locations=locations_results, sensors=sensors_results,)
+    elif request.method == "POST":
+        # set vars:
+        from_date = ''
+        to_date = ''
+        sensor = ''
+        return redirect("/results", from_date=from_date, to_date=to_date, sensor=sensor)
 
-@app.route('/results')
+@app.route('/results', methods=["GET"])
 def result_info(from_date, to_date, sensor):
     forecasts_query = "SELECT * FROM Forecasts;"
     readings_query = "SELECT * FROM Readings;"
@@ -33,18 +40,25 @@ def result_info(from_date, to_date, sensor):
 
     return render_template("results.j2", forecasts=forecasts_results, readings=readings_results)
 
-@app.route('/editsensors')
+@app.route('/editsensors', methods=["POST", "GET"])
 def edit_sensors():
-    edit_sensor_query = f"SELECT * FROM Sensors;"
-    edit_sensor_results = db.execute_query(db_connection=db_connection, query=edit_sensor_query).fetchall()
-    return render_template("editsensors.j2", model_string=edit_sensor_results)
+    if request.method == "GET":
+        edit_sensor_query = f"SELECT * FROM Sensors;"
+        edit_sensor_results = db.execute_query(db_connection=db_connection, query=edit_sensor_query).fetchall()
+        return render_template("editsensors.j2", sensors=edit_sensor_results)
+    elif request.method == "POST":
+        # sql commands to update or add
+        return redirect("/")
 
-@app.route('/editlocations')
+@app.route('/editlocations', methods=["POST", "GET"])
 def edit_locations():
-    edit_loc_query = f"SELECT * FROM Locations;"
-    edit_loc_results = db.execute_query(db_connection=db_connection, query=edit_loc_query).fetchall()
-    return render_template("editsensors.j2", model_string=edit_loc_results)
-
+    if request.method == "GET":
+        edit_loc_query = f"SELECT * FROM Locations;"
+        edit_loc_results = db.execute_query(db_connection=db_connection, query=edit_loc_query).fetchall()
+        return render_template("editlocations.j2", locations=edit_loc_results)
+    elif request.method == "POST":
+        # sql commands to update or add
+        return redirect("/")
     
 
 if __name__ == "__main__":
